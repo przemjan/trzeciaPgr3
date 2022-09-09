c2d12b4581fae02c6cb90ba73266fae1502f9258
przemjan <przemjanisz2@gmail.com>
2022-09-09 14:37:13 +0200

    Commit nr 1

---
+++ src/Zmienne.java
@@ -0,0 +1,35 @@
+public class Zmienne {
+    public static void main(String[] args) {
+
+        int numerButa;
+        numerButa = 123;
+
+        double number2, number3, number4;
+
+        double number5 = 123.7;
+
+        System.out.println("Liczba: " + numerButa);
+
+        String text = "Jakiś \"fajny\" \n\t napis";
+
+        char letter = 'a';
+        boolean twierdzenie = true; //false
+
+
+        if(twierdzenie){
+            System.out.println("Sukces");
+        }
+
+        System.out.println("Mój napis " + text + " "+ numerButa);
+
+        int podatek;
+
+        int factor = 4;
+        podatek = numerButa % factor;
+
+        System.out.println("Mój \\napis " + text + " "+ numerButa);
+        System.out.print("Mój \"napis\" \n" + text + " "+ numerButa);
+        System.out.print("Mój napis \t" + text + " "+ numerButa);
+
+    }
+}
