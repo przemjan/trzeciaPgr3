d68ffc4c681fcac238a70ea35941d577b8e9f477
przemjan <przemjanisz2@gmail.com>
2022-09-12 10:32:32 +0200

    Commit nr 10

--- src/randomGenerator/LiczbyPseudolosowe.java
+++ src/randomGenerator/LiczbyPseudolosowe.java
@@ -26,5 +26,9 @@ public class LiczbyPseudolosowe {
         losowaLiczba3 = losowanie.nextDouble();
 
         System.out.println ("RNG to " + losowaLiczba3 );
+
+        // https://github.com/przemjan/trzeciaPgr3
+        //1,2,12,14
+        //zadania7 - 2 z 3, 6, 7, 8 ,9,10, 14, 17 z 18, 19||21||22
     }
 }
